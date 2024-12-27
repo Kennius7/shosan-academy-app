69bc51b4e8ddd8be862011d7860d0c37074ebea6
Kennius7 <ogbogukenny@yahoo.com>
2024-12-27 15:02:31 +0100

    server config creation - 04

--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 
 
-export default function handler(req, res) {
+export default function handler(req, res, next) {
     const { name } = req.query;
     const message = name ? `Hello, ${name}` : "Hello Guest";
     res.status(200).json({ message: message });
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+    next();
 
     if (req.method === "POST") {
         const { email, password } = req.body;
