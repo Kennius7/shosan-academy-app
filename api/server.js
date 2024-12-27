26c299931ebdfb0a9c099ebd30c605366c26347f
Kennius7 <ogbogukenny@yahoo.com>
2024-12-27 16:25:10 +0100

    server config creation - 17

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
 
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
 
     if (req.method === "OPTIONS") {
         res.status(200).end();
